0e0f1275b29e2b2d0495bc151b8fd2de54c5dca7
OfficePlata <150496622+OfficePlata@users.noreply.github.com>
2025-10-02 00:43:22 +0900

    Update app-v3.js

--- app-v3.js
+++ app-v3.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // --- ▼▼▼ 最終設定項目 ▼▼▼ ---
     const MAIN_LIFF_ID = "2008199273-3ogv1YME";
     // 【重要】デプロイしたGASのURLをここに設定してください
-    const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzwaGwWbPhLwFr_Ft7I11FuM7jq0XmZ_nDxZ7VQqpk5msnvo6pKLM4avxIwH9mOXbMY/exec";
+    const GAS_API_URL = "https://script.google.com/macros/s/AKfycbw-t5n0nfXrFzOt5L5tw1NTDnJe7zEvQKBi9cEh4jVknjRk2qeSj3QhLS8nzYPXf70e/exec";
     // --- ▲▲▲ 最終設定項目 ▲▲▲ ---
     // (これより下の部分は変更不要です)
     let menuData = [];
